65717d2a518d51a27d74b2d05ad12655e26b12aa
anggafr12 <117493988+anggafr12@users.noreply.github.com>
2023-11-27 09:48:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include 'header.php';?>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="300" height="300"
+                src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/03/23/Potret-Puput-Nastiti-Devi-3448977465.jpg"
+                alt="">
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+    </div>
+
+
+    <div class="container text-center" id="about">
+
+        <div class="row">
+            <div class="col">
+                <h1>About</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quos repellat alias repudiandae
+                temporibus cupiditate commodi tenetur ab molestias nostrum amet optio architecto suscipit inventore,
+                possimus nulla ex accusamus! Distinctio!
+            </div>
+            <div class="col text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, quos repellat alias repudiandae
+                temporibus cupiditate commodi tenetur ab molestias nostrum amet optio architecto suscipit inventore,
+                possimus nulla ex accusamus! Distinctio!
+            </div> 
+        </div>
+
+    </div>
+
+
+
+    <div class="card-deck m-5">
+        <div class="card">
+          <img class="card-img-top" src="https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/24/2022/08/Biodata-Profil-Kim-Tae-Hee-Terlengkap-dan-Terbaru.jpg?width=450&quality=90" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/24/2022/08/Biodata-Profil-Kim-Tae-Hee-Terlengkap-dan-Terbaru.jpg?width=450&quality=90" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/24/2022/08/Biodata-Profil-Kim-Tae-Hee-Terlengkap-dan-Terbaru.jpg?width=450&quality=90" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+      
+ <?php include 'footer.php';?>
